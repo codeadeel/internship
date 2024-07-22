588ff3d6cd90fa6c70e75ca843eaa2f60fa1fda7
Eshmal Rumman <eshmalrumman3@gmail.com>
2024-07-23 00:44:24 +0500

    something

--- regression.py
+++ regression.py
@@ -1,4 +1,4 @@
-import matplotlib as mp
+import matplotlib.pyplot as mp
 import pandas as pan
 import math 
 
@@ -8,10 +8,28 @@ class data:
         self.allScaledFeatures = []
         # h(x) = theta0 + (theta1)(x1^2) + (theta2)(x2^3) + (theta3)(ln(x3)) + (theta4)(e^x4) + (theta5)(x5^4)
         self.theta = [0.034, -0.089, 0.067, -0.043, 0.021, -0.056] 
-        self.alpha = 0.01
+        self.alpha = 0.0001
+        self.epochs = 50
+        self.totalLen = 0
+        self.trainingLen = 0
+        self.trainingData = []
+        self.validLen = 0
+        self.validData = []
+        self.testLen = 0
+        self.testData = []
+        self.batches = []
         
     def openCSV(self):
         print(self.file)
+        self.totalLen = len(self.file)
+        self.trainingLen = int(0.7 * self.totalLen)
+        self.validLen = self.testLen = int(0.15 * self.totalLen)
+        
+        self.trainingData = self.file[ :self.trainingLen]
+        self.validData = self.file[self.trainingLen : self.trainingLen + self.validLen]
+        self.testData = self.file[self.trainingLen + self.validLen: ]
+        
+        #print(f"Total: {self.totalLen}, Training: {self.trainingLen}, Valid: {self.validLen}, Test: {self.testLen}")
         
     def scaling(self, feature):
         features = self.file[feature]
@@ -31,6 +49,13 @@ class data:
         self.allScaledFeatures.append(featureScaled)   
         print("Scaled ", feature, ": ",featureScaled, "\n")
         #print(f"Min: {minVal}, Max: {maxVal}")
+        
+    def createBatches(self):
+        batchSize = int(0.1 * self.trainingLen)
+        self.batches = [self.trainingData[i : i + batchSize] for i in range(0, self.trainingLen, batchSize)]
+        if len(self.batches[-1]) < batchSize:
+            lastBatch = self.batches.pop()
+            self.batches[-1] = pan.concat([self.batches[-1], lastBatch])
         
     def calcHypo(self, exampleNum): # exampleNum -> 0-14
         hypo =  (self.theta[0])*(1) + \
@@ -57,13 +82,39 @@ class data:
         return temp3
     
     def gradient(self):
-        for _ in range(9000):
-            tempThetas = []
-            for i in range(len(self.theta)):
-                temp4 = self.theta[i] - (self.alpha)*(self.calcCost(i))
-                tempThetas.append(temp4)    
-            self.theta = tempThetas
-    
+        epochLosses = []
+        for epoch in range(self.epochs):
+            loss = 0
+            batchLosses= []
+            for batch in self.batches:
+                batch_data = batch.reset_index(drop=True)
+                self.file = batch_data
+                temp4 = 0
+                for _ in range(100):
+                    temp5 = 0
+                    tempThetas = []
+                    for i in range(len(self.theta)):
+                        temp4 = self.theta[i] - (self.alpha)*(self.calcCost(i))
+                        tempThetas.append(temp4)   
+                        temp5 += self.calcHypo(i) 
+                    batchLosses.append(temp5)
+                    self.theta = tempThetas
+                loss = sum(batchLosses) / len(batchLosses)
+                epochLosses.append(loss)
+            
+            print(f"Epoch {epoch + 1}, Loss: {loss}")
+        
+        self.plotLoss(epochLosses)
+        
+    def plotLoss(self, losses):
+        mp.figure(figsize=(10, 5))
+        mp.plot(losses, label='Training Loss')
+        mp.xlabel('Epochs')
+        mp.ylabel('Loss')
+        mp.title('Loss over Epochs')
+        mp.legend()
+        mp.show()
+        
     def MAIN(self): 
         self.openCSV()
         self.scaling("price")
@@ -73,8 +124,9 @@ class data:
         self.scaling("stories")
         self.scaling("parking")
         
-        print(f"Thetas before gradient descent: {self.theta}")
+        #print(f"Thetas before gradient descent: {self.theta}")
         
+        self.createBatches()
         self.gradient()
         
         print(f"Thetas after gradient descent: {self.theta}")
