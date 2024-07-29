284355332ce5fe5636caa2daf7ea516d858827f6
Eshmal Rumman <eshmalrumman3@gmail.com>
2024-07-29 21:45:51 +0500

    FINAL CODE FOR LINEAR REGRESSION FROM SCRATCH AND WITH LIBRARIES

--- linearModel.py
+++ linearModel.py
@@ -3,44 +3,252 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LinearRegression 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error, log_loss
+import json as storage
 
-file = pd.read_csv("E:/progamming/Machine learning/internship/housing2.csv")
-file['basement'] = file['basement'].map({'yes': 1, 'no': 0})
 
-sns.lmplot(x ="area", y ="price", data = file, order = 1, ci = None) 
-plt.show()
-sns.lmplot(x ="bedrooms", y ="price", data = file, order = 1, ci = None) 
-plt.show()
-sns.lmplot(x ="bathrooms", y ="price", data = file, order = 1, ci = None) 
-plt.show()
-sns.lmplot(x ="stories", y ="price", data = file, order = 1, ci = None) 
-plt.show()
-sns.lmplot(x ="parking", y ="price", data = file, order = 1, ci = None) 
-plt.show()
-sns.lmplot(x ="basement", y ="price", data = file, order = 1, ci = None) 
-plt.show()
+class ModelUsingLibs:
+    """
+        A class to manage and analyze housing data using machine learning models in libraries.
 
-Y = np.array(file['price']).reshape(-1, 1) 
-X = np.array(file[['area', 'bedrooms', 'bathrooms', 'stories', 'parking', 'basement']])
+        Attributes:
+        ----------
+        file : DataFrame
+            The housing data read from a CSV file.
+        featureNames : list
+            The names of the features in the dataset.
+        scaledFeatures : ndarray
+            The features of the dataset after scaling.
+        reg : object
+            The LinearRegression model object.
+        thetas : list
+            The coefficients of the linear regression model.
+        trainX, trainY, validX, validY, testX, testY : ndarray
+            The training, validation, and test datasets and their labels.
+    """
+    def __init__(self):
+        """
+            Initializes the ModelUsingLibs class with default values and reads the CSV file.
+        """
+        self.file = pd.read_csv("E:/progamming/Machine learning/internship/housing2.csv")
+        self.featureNames = ["price", "area", "bedrooms", "bathrooms", "stories", "parking", "basement"]
+        self.scaledFeatures = []
+        # self.X_train = self.X_test = self.y_train = self.y_test = 0
+        self.reg = 0
+        self.thetas = []
+        
+        self.trainX = self.trainY = []
+        self.validX = []
+        self.validY = []
+        self.testX = []
+        self.testY = []
+    
+    def readCSV(self):
+        """
+            Converts the 'basement' column values to numerical format and prints the CSV file.
+        """
+        self.file['basement'] = self.file['basement'].map({'yes': 1, 'no': 0})
+        print(self.file)
+        
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3) 
+    def scaleData(self):
+        """
+            Scales the feature columns using StandardScaler and combines with the 'basement' column.
+        """
+        scale = StandardScaler()
+        X = self.file[['price', 'area', 'bedrooms', 'bathrooms', 'stories', 'parking']]
+        self.scaledFeatures = scale.fit_transform(X)
+        
+        basement_column = self.file['basement'].values.reshape(-1, 1)
+        self.scaledFeatures = np.hstack((self.scaledFeatures, basement_column))
+        print("Scaled features: ", self.scaledFeatures)
+    
+    def splittingData(self):
+        """
+            Splits the data into training, validation, and test sets.
+        """
+        Y = self.scaledFeatures[:, 0]
+        X = self.scaledFeatures[:, 1:6]
+        X_train_valid, self.testX, y_train_valid, self.testY = train_test_split(X, Y, test_size=0.15, random_state=42)
+        self.trainX, self.validX, self.trainY, self.validY = train_test_split(X_train_valid, y_train_valid, test_size=0.176, random_state=42)
+        
+        
+        # self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+    
+    def graphBwFeatures(self):
+        """
+            Plots the relationships between features and the target variable using seaborn.
+        """
+        df_scaled = pd.DataFrame(self.scaledFeatures, columns = self.featureNames)
+        sns.lmplot(x ="area", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+        sns.lmplot(x ="bedrooms", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+        sns.lmplot(x ="bathrooms", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+        sns.lmplot(x ="stories", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+        sns.lmplot(x ="parking", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+        sns.lmplot(x ="basement", y ="price", data = df_scaled, ci = None) 
+        plt.show()
+    
+    def calcCost(self):
+        """
+            Calculates the cost function (Mean Squared Error) for the training data.
+            
+            Returns:
+            -------
+            float
+                The computed cost value.
+        """
+        preds = self.reg.predict(self.trainX)
+        mse = mean_squared_error(self.trainY, preds)
+        mse = mse / 2
+        return mse
+        
+        # preds = self.reg.predict(self.X_train)
+        # mse = mean_squared_error(self.y_train, preds)
+        # mse = mse / 2
+        # return mse
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+    def training(self):
+        """
+            Trains the LinearRegression model on the training data and calculates and prints the training cost.
+        """
+        self.reg = LinearRegression().fit(self.trainX, self.trainY)
+        cost = self.calcCost()
+        print(f"Cost from training: {cost}")
+        
+        # self.reg = LinearRegression().fit(self.X_train, self.y_train)
+        # cost = self.calcCost()
+        # print(f"Cost from training: {cost}")
+    
+    def validation(self):
+        """
+            Validates the model on the validation dataset and prints validation loss and accuracy.
+        """
+        validLoss = self.Loss(self.validX, self.validY)
+        validAccu = self.calcAccuracy(self.validX, self.validY)
+        print(f"Validation Loss: {validLoss}, Validation Accuracy: {validAccu * 100}")
+    
+    def testing(self):
+        """
+            Tests the model on the test dataset and prints testing loss and accuracy.
+        """
+        testLoss = self.Loss(self.testX, self.testY)
+        testAccu = self.calcAccuracy(self.testX, self.testY)
+        print(f"Testing Loss: {testLoss}, Testing Accuracy: {testAccu * 100}")
+    
+    def Loss(self, X, Y):
+        """
+            Calculates the loss (Mean Squared Error) for a given dataset.
+            
+            Parameters:
+            X : ndarray
+                Features of the dataset.
+            Y : ndarray
+                Target feature(price) of the dataset.
+            
+            Returns:
+            -------
+            float
+                The computed loss value.
+        """
+        preds = self.reg.predict(X)
+        mse = mean_squared_error(Y, preds)
+        mse = mse / 2
+        return mse
+    
+    def calcAccuracy(self, X, Y):
+        """
+            Calculates the accuracy of the model on a given dataset.
+            
+            Parameters:
+            X : ndarray
+                Features of the dataset.
+            Y : ndarray
+                Target feature(price) of the dataset.
+            
+            Returns:
+            -------
+            float
+                The computed accuracy value.
+        """
+        accuracy = self.reg.score(X, Y)
+        return accuracy
+        
+    def thetaValues(self):
+        """
+            Extracts and prints the model's theta values (intercept and coefficients).
+        """
+        inter = float(self.reg.intercept_)
+        theta = self.reg.coef_
+        self.thetas.append(inter) 
+        for i in range(len(theta)):
+            self.thetas.append(float(theta[i]))
+        print(self.thetas) 
+    
+    def storeJson(self):
+        """
+            Stores the model parameters in a JSON file.
+        """
+        data = {'Thetas' : self.thetas, 
+                'TrainingLoss' : self.calcCost(),
+                'ValidationLoss': self.Loss(self.validX, self.validY), 
+                'ValidationAccuracy' : self.calcAccuracy(self.validX, self.validY) * 100,
+                'TestingLoss' : self.Loss(self.testX, self.testY),
+                'TestingAccuracy' : self.calcAccuracy(self.testX, self.testY) * 100}
+        
+        with open("ModelWithLibsValues.json", "w") as file3:
+            storage.dump(data, file3)
 
-print(f"The Coefficients are: {model.coef_}")
-print(f"The y intercept: {model.intercept_}")
-print("Score: ", model.score(X_test, y_test))
-
-y_pred = model.predict(X_test)
-
-feature_index = file.columns.get_loc('area')
-plt.scatter(X_test[:, feature_index], y_test, color='b', label='Actual')
-plt.scatter(X_test[:, feature_index], y_pred, color='k', label='Predicted')
-plt.xlabel('Area')
-plt.ylabel('Price')
-plt.title('Actual vs Predicted Prices')
-plt.legend()
-plt.show()
+        print("Everything stored successfully!")
+    
+    def comparison(self):
+        """
+            Compares the model performance with another model based on stored parameters.
+        """
+        with open("thetaValueAfterTraining.json", "r") as file4:
+            data = storage.load(file4)
+        
+        othertrainLoss = data['TrainingLoss']
+        othervalLoss = data['ValidationLoss']
+        othervalidAccu = data['ValidationAccuracy']
+        othertestLoss = data['TestingLoss']
+        othertestAccu = data['TestingAccuracy']
+        
+        print(f"\nValidation Accuracy WITH using libraries: {self.calcAccuracy(self.validX, self.validY) * 100}")
+        print(f"Validation Accuracy WITHOUT using libraries: {othervalidAccu}\n")
+        
+        print(f"Testing Accuracy WITH using libraries: {self.calcAccuracy(self.testX, self.testY) * 100}")
+        print(f"Testing Accuracy WITHOUT using libraries: {othertestAccu}\n")
+        
+        print(f"Training Loss WITH using libraries: {self.calcCost()}")
+        print(f"Training Loss WITHOUT using libraries: {othertrainLoss}\n")
+        
+        print(f"Validation Loss WITH using libraries: {self.Loss(self.validX, self.validY)}")
+        print(f"Validation Loss WITHOUT using libraries: {othervalLoss}\n")
+        
+        print(f"Testing Loss WITH using libraries: {self.Loss(self.testX, self.testY)}")
+        print(f"Testing Loss WITHOUT using libraries: {othertestLoss}\n\n")
+    
+        
+    
+# Creating an instance of ModelUsingLibs and executing the methods    
+linear = ModelUsingLibs()
+linear.readCSV()
+linear.scaleData()
+linear.splittingData()
+# linear.graphBwFeatures()
+linear.training()
+linear.validation()
+linear.testing()
+linear.thetaValues()
+linear.storeJson()
+linear.comparison()
+linear.trainingLogistic()
+linear.validationLogistic()
+linear.testingLogistic()
--- regression.py
+++ regression.py
@@ -4,12 +4,65 @@ import json as storage
 import math 
 
 class data:
+    """
+            A class to manage and analyze housing data using linear regression.
+
+            Attributes:
+            ----------
+            file : DataFrame
+                The housing data read from a CSV file.
+            allScaledFeatures : list
+                A list containing scaled features for all data.
+            featureNames : list
+                The names of the features used in the model.
+            theta : list
+                The coefficients for the linear regression model.
+            trainLoss : float
+                The loss on the training data.
+            valLoss : float
+                The loss on the validation data.
+            testLoss : float
+                The loss on the test data.
+            validAccu : float
+                The accuracy on the validation data.
+            testAccu : float
+                The accuracy on the test data.
+            Mean : dict
+                The mean values of the features used for scaling.
+            Deviation : dict
+                The standard deviation values of the features used for scaling.
+            alpha : float
+                The learning rate for gradient descent.
+            epochs : int
+                The number of epochs for training.
+            totalLen : int
+                The total number of data samples.
+            trainingLen : int
+                The number of samples in the training set.
+            validLen : int
+                The number of samples in the validation set.
+            testLen : int
+                The number of samples in the test set.
+            trainingData : list
+                The training data samples.
+            validData : list
+                The validation data samples.
+            testData : list
+                The test data samples.
+            batches : list
+                The batches created for training.
+    """
     def __init__(self):
+        """
+            Initializes the Data class with default values and reads the CSV file.
+        """
         self.file = pan.read_csv("housing2.csv")
         self.allScaledFeatures = []
         self.featureNames = ["price", "area", "bedrooms", "bathrooms", "stories", "parking", "basement"]
         # h(x) = theta0 + (theta1)(x1) + (theta2)(x2) + (theta3)(x3) + (theta4)(x4) + (theta5)(x5) + (theta6)(x6)
         self.theta = [0.034, -0.089, 0.067, -0.043, 0.021, -0.056, 0.035] 
+        self.trainLoss = self.valLoss = self.testLoss = self.validAccu = self.testAccu = 0
+        self.Mean = self.Deviation = {}
         self.alpha = 0.0001
         self.epochs = 50
         self.totalLen = 0
@@ -22,6 +75,9 @@ class data:
         self.batches = []
         
     def openCSV(self):
+        """
+            Opens the CSV file and calculates the lengths for training, validation, and test sets.
+        """
         print(self.file)
         self.totalLen = len(self.file)
         self.trainingLen = int(0.7 * self.totalLen)
@@ -30,6 +86,9 @@ class data:
         #print(f"Total: {self.totalLen}, Training: {self.trainingLen}, Valid: {self.validLen}, Test: {self.testLen}")
         
     def seperateData(self):
+        """
+            Separates the data into training, validation, and test datasets.
+        """
         self.trainingData = [
             {self.featureNames[ind]: self.allScaledFeatures[ind][i] for ind in range(len(self.featureNames))}
             for i in range(self.trainingLen)
@@ -46,6 +105,13 @@ class data:
         ]
         
     def scaling(self, feature):
+        """
+            Scales a given feature using normalization or standardization.
+            
+            Parameters:
+            feature : str
+                The name of the feature to be scaled.
+        """
         features = self.file[feature]
         featureScaled = []
         number = len(features)
@@ -57,6 +123,9 @@ class data:
                     featureScaled.append(1)
                 elif features[i] == "no":
                     featureScaled.append(0)
+            
+            self.Mean[feature] = mean
+            self.Deviation[feature] = dev
         else:
             for i in range(number):
                 mean += features.iloc[i]
@@ -70,9 +139,11 @@ class data:
             for k in range(number):
                 temp = (features.iloc[k] - mean) / dev
                 featureScaled.append(temp)
+                
+            self.Mean[feature] = mean
+            self.Deviation[feature] = dev
             
         self.allScaledFeatures.append(featureScaled)  
-        
         # maxVal = features.max()
         # minVal = features.min()
         # for i in range(len(features)):
@@ -87,6 +158,15 @@ class data:
         #print(f"Min: {minVal}, Max: {maxVal}")
         
     def featureRelations(self, featureX, featureY):
+        """
+            Plots the relationship between two features.
+            
+            Parameters:
+            featureX : int
+                Index of the first feature.
+            featureY : int
+                Index of the second feature.
+        """
         X = [exampleNum[self.featureNames[featureX]] for exampleNum in self.trainingData]
         Y = [exampleNum[self.featureNames[featureY]] for exampleNum in self.trainingData]
         
@@ -99,6 +179,9 @@ class data:
         mp.show()
         
     def createBatches(self):
+        """
+            Creates batches of training data for mini-batch gradient descent.
+        """
         batchSize = int(0.1 * self.trainingLen)
         self.batches = [self.trainingData[i : i + batchSize] for i in range(0, self.trainingLen, batchSize)]
         if len(self.batches[-1]) < batchSize:
@@ -106,6 +189,17 @@ class data:
             self.batches[-1].extend(lastBatch)
         
     def calcHypo(self, exampleNum):
+        """
+            Calculates the hypothesis function h(x) for a given example.
+            
+            Parameters:
+            exampleNum : dict
+                A dictionary containing feature values for the example.
+            
+            Returns:
+            float
+                The computed hypothesis value.
+        """
         hypo =  (self.theta[0])*(1) + \
                 (self.theta[1])*(exampleNum[self.featureNames[1]]) + \
                 (self.theta[2])*(exampleNum[self.featureNames[2]]) + \
@@ -117,6 +211,17 @@ class data:
         return hypo
 
     def calcCost(self, thetaNum):
+        """
+            Calculates the gradient of the cost function with respect to a given theta.
+            
+            Parameters:
+            thetaNum : int
+                The index of the theta coefficient.
+            
+            Returns:
+            float
+                The computed gradient value.
+        """
         temp3 = 0
         rows = len(self.trainingData)
         if thetaNum == 0:
@@ -133,6 +238,13 @@ class data:
         return temp3
     
     def calcValidLoss(self):
+        """
+            Calculates the loss on the validation dataset.
+            
+            Returns:
+            float
+                The computed validation loss.
+        """
         temp = 0
         for exampleNum in self.validData:
             temp += ((self.calcHypo(exampleNum)) - exampleNum[self.featureNames[0]]) ** 2
@@ -141,6 +253,13 @@ class data:
         return temp
     
     def calcAccuracy(self):
+        """
+            Calculates the accuracy of the model on the validation dataset.
+            
+            Returns:
+            float
+                The computed validation accuracy in percentage.
+        """
         correct_predictions = 0
         total_predictions = len(self.validData)
         for exampleNum in self.validData:
@@ -152,6 +271,9 @@ class data:
         return accuracy
     
     def gradient(self):
+        """
+            Performs gradient descent to update the theta values and tracks loss and accuracy over epochs.
+        """
         epochLosses = []
         Accuracies = []
         validLoss = []
@@ -182,10 +304,25 @@ class data:
             val_accuracy = self.calcAccuracy()
             Accuracies.append(val_accuracy)
             print(f"Epoch {epoch + 1}, Training Loss: {loss}, Validation Accuracy: {val_accuracy}, validation loss: {val_loss}")
+            
         self.plotLoss(epochLosses, validLoss)
         self.plotAccuracy(Accuracies)
         
+        self.trainLoss = loss
+        self.valLoss = val_loss
+        self.validAccu = val_accuracy
+        
+        
     def plotLoss(self, losses, loss2):
+        """
+            Plots the training and validation loss over epochs.
+            
+            Parameters:
+            losses : list
+                List of training loss values.
+            loss2 : list
+                List of validation loss values.
+        """
         mp.figure(figsize=(10, 5))
         mp.plot(losses, label='Training Loss')
         mp.plot(loss2, label='Validation Loss')
@@ -196,6 +333,13 @@ class data:
         mp.show()
         
     def plotAccuracy(self, accuracy):
+        """
+            Plots the validation accuracy over epochs.
+            
+            Parameters:
+            accuracy : list
+                List of validation accuracy values.
+        """
         mp.figure(figsize=(10, 5))
         mp.plot(accuracy, label='Validation Accuracy')
         mp.xlabel('Epochs')
@@ -205,20 +349,52 @@ class data:
         mp.show()
         
     def storeParas(self):
-        data = self.theta
-
+        """
+            Stores the parameters of the model to a JSON file.
+        """
+        data = {'Thetas' : self.theta, 
+                'TrainingLoss' : self.trainLoss,
+                'ValidationLoss' : self.valLoss,
+                'ValidationAccuracy' : self.validAccu,
+                'TestingLoss' : self.testLoss,
+                'TestingAccuracy' : self.testAccu,
+                'Mean' : self.Mean,
+                'Deviation' : self.Deviation}
+        
         with open("thetaValueAfterTraining.json", "w") as file2:
             storage.dump(data, file2)
 
-        print("Thetas stored successfully after training!")
+        print("Everything stored successfully after training!")
     
     def loadParas(self):
+        """
+            Loads the parameters of the model from a JSON file.
+        """
         with open("thetaValueAfterTraining.json", "r") as file3:
-            self.theta = storage.load(file3)
-            
-        print("theta from json file: ", self.theta, "\n")
+            data = storage.load(file3)
+        
+        self.theta = data['Thetas']
+        self.trainLoss = data['TrainingLoss']
+        self.valLoss = data['ValidationLoss']
+        self.validAccu = data['ValidationAccuracy']
+        self.testLoss = data['TestingLoss']
+        self.testAccu = data['TestingAccuracy']
+        self.Mean = data['Mean']
+        self.Deviation = data['Deviation']
+             
+        print("theta from json file: ", self.theta)
+        print("trainging loss from json file: ", self.trainLoss)
+        print("validation loss from json file: ", self.valLoss)
+        print("validation accuracy from json file: ", self.validAccu)
+        print("testing loss from json file: ", self.testLoss)
+        print("testing accuracy from json file: ", self.testAccu)
+        print("Mean from json file: ", self.Mean)
+        print("Deviation from json file: ", self.Deviation, "\n")
         
     def testModel(self):
+        """
+            Tests the model on the test dataset and prints accuracy and mean squared error (MSE).
+        """
         correct_predictions = 0
         total_predictions = len(self.testData)
         mse = 0
@@ -227,44 +403,67 @@ class data:
             actual = exampleNum[self.featureNames[0]]
             if abs(prediction - actual) < 0.5:
                 correct_predictions += 1
-        accuracy = (correct_predictions / total_predictions) * 100
+        accu = (correct_predictions / total_predictions) * 100
         
         for exampleNum in self.testData:
             mse += (self.calcHypo(exampleNum) - exampleNum[self.featureNames[0]]) ** 2
         mse = mse / (2 * len(self.testData))
-        rmse = math.sqrt(mse)
         
-        print(f"Accuracy after testing data: {accuracy}")
+        print(f"Accuracy after testing data: {accu}")
         print(f"MSE after testing data: {mse}")
-        print(f"RMSE after testing data: {rmse}")
+        
+        self.testAccu = accu
+        self.testLoss = mse
         
     def userIn(self):
+        """
+            Takes user input for feature values, scales them, and predicts the price based on the model and tells if the price is high or lpw.
+        """
+        priceAvg = self.file['price'].median()
         area = int(input("Enter Area: "))
-        beds = int(input("Enter Area: "))
-        baths = int(input("Enter Area: "))
-        stories = int(input("Enter Area: "))
-        parking = int(input("Enter Area: "))
-        basement = int(input("Enter Area: "))
+        beds = int(input("Enter Bedrooms: "))
+        baths = int(input("Enter Bathrooms: "))
+        stories = int(input("Enter Stories: "))
+        parking = int(input("Enter Parking: "))
+        basement = int(input("Enter Basement: "))
         
-        hypo =  (self.theta[0])*(1) + \
+        area = (area - self.Mean['area']) / self.Deviation['area']
+        beds = (beds - self.Mean['bedrooms']) / self.Deviation['bedrooms']
+        baths = (baths - self.Mean['bathrooms']) / self.Deviation['bathrooms']
+        stories = (stories - self.Mean['stories']) / self.Deviation['stories']
+        parking = (parking - self.Mean['parking']) / self.Deviation['parking']
+        
+        answer =  (self.theta[0])*(1) + \
                 (self.theta[1])*(area) + \
                 (self.theta[2])*(beds) + \
                 (self.theta[3])*(baths) + \
                 (self.theta[4])*(stories) + \
                 (self.theta[5])*(parking) + \
                 (self.theta[6])*(basement)
+                
+        price = (answer * self.Deviation['price']) + self.Mean['price']
+        print(f"the price is: {price}")
+        if (price >= priceAvg):
+            print("Price is HIGH")
+        else:
+            print("Price is LOW")
         
-    def MAIN(self): 
+    def MAIN(self):
+        """
+            Main function to execute the data processing, model training, and testing.
+        """ 
         self.openCSV()
-        self.scaling("price")
-        self.scaling("area")
-        self.scaling("bedrooms")
-        self.scaling("bathrooms")
-        self.scaling("stories")
-        self.scaling("parking")
-        self.scaling("basement")
+        # self.scaling("price")
+        # self.scaling("area")
+        # self.scaling("bedrooms")
+        # self.scaling("bathrooms")
+        # self.scaling("stories")
+        # self.scaling("parking")
+        # self.scaling("basement")
         
-        self.seperateData()
+        #print(self.Mean, " : ", self.Deviation)
+        
+        # self.seperateData()
         
         # self.featureRelations(1, 0)     #since its a 2D list, so we are referencing the features by their list number in allScaledFeature list
         # self.featureRelations(2, 0)
@@ -273,17 +472,18 @@ class data:
         # self.featureRelations(5, 0)
         # self.featureRelations(6, 0)
         
-        print(f"Thetas before gradient descent: {self.theta}")
+        # print(f"Thetas before gradient descent: {self.theta}")
         
-        self.createBatches()        
-        #self.gradient()
+        # self.createBatches()        
+        # self.gradient()
         
-        #self.storeParas()
+        # self.testModel()
+        
+        # self.storeParas()
+        # print(f"Thetas after gradient descent: {self.theta}")
         
         self.loadParas()
-        self.testModel()
-        
-        #print(f"Thetas after gradient descent: {self.theta}")
+        self.userIn()
         
         
      
